7aa25298a921c3b9eb3cc43a70f1ca2e993fd1a9
alragi0 <140979427+alragi0@users.noreply.github.com>
2023-09-03 02:45:26 +0300

    Update info.py

--- info.py
+++ info.py
@@ -1,4 +1,4 @@
-token = '6139441987:AAGlGJNkCpKCUizapsl-jVRbcO7oogWvJBc'
+token = '6109895485:AAE43imQ2y0W_yDx5B_Fsdod_SWt7MyrKQg'
 sudo_username = 'YYNNXXXX'
 user_bot = 'EEObot'
 sudo_id = 6699312679
