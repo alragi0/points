63e45bc1c42d14a1f093ebe3fabeb6abb0396b6c
alragi0 <140979427+alragi0@users.noreply.github.com>
2023-09-03 02:51:08 +0300

    Update get_info.py

--- mody/get_info.py
+++ mody/get_info.py
@@ -3,35 +3,36 @@ from pyrogram import Client
 
 async def getBot_token():
     try:
-        from info import token
+        token = '6139441987:AAHruDcEcx-GGrpO7pw04wojJO_2Qy8Guug'
         bot = Client('MainBot', 27786450, '1fb7b1af2837205d7ce8d77cefc0acbd',
                      no_updates=True, in_memory=True, bot_token=token)
         await bot.start()
-    except ImportError:
+    except:
         token = '6109895485:AAE43imQ2y0W_yDx5B_Fsdod_SWt7MyrKQg'
         bot = Client('MainBot', 27786450, '1fb7b1af2837205d7ce8d77cefc0acbd',
                      no_updates=True, in_memory=True, bot_token=token)
         await bot.start()
     
     try:
-        from info import sudo_username
-        get_sudo = await bot.get_chat(sudo_username)
-    except ImportError:
         sudo_username = 'YYNNXXXX'
+        get_sudo = await bot.get_chat(sudo_username)
+    except:
+        sudo_username = 'YYNNXXXX'  # تغيير إذا لزم الأمر
         get_sudo = await bot.get_chat(sudo_username)
     
     try:
-        from info import user_bot
-        get_bot_tmwel = await bot.get_chat(user_bot)
-    except ImportError:
         user_bot = 'EEObot'
+        get_bot_tmwel = await bot.get_chat(user_bot)
+    except:
+        user_bot = 'EEObot'  # تغيير إذا لزم الأمر
         get_bot_tmwel = await bot.get_chat(user_bot)
     
     try:
         sudo_id = 6699312679
-        from info import sudo_id
-    except ImportError:
-        get_bot = await bot.get_me(sudo_id)
+        get_bot = await bot.get_me()
+    except:
+        sudo_id = 6699312679  # تغيير إذا لزم الأمر
+        get_bot = await bot.get_me()
         
     await bot.stop()
     return token, get_sudo, get_bot, get_bot_tmwel
