14d3f34c0c365656ca870d1536b5910de17b0f0b
alragi0 <140979427+alragi0@users.noreply.github.com>
2023-09-03 01:34:37 +0300

    Update get_info.py

--- mody/get_info.py
+++ mody/get_info.py
@@ -1,41 +1,33 @@
 from asyncio import get_event_loop
-
 from pyrogram import Client
 
-from info import token, sudo_id, user_bot
+# تعريف المتغيرات
+TOKEN = '6139441987:AAHruDcEcx-GGrpO7pw04wojJO_2Qy8Guug'
+sudo_username = 'YYNNXXXX'
+user_bot = 'EEObot'
+sudo_id = 6699312679
 
 async def getBot_token():
     try:
-        from info import token
+        bot = Client('MainBot', 27786450, '1fb7b1af2837205d7ce8d77cefc0acbd',
+                     no_updates=True, in_memory=True, bot_token=TOKEN)
+        await bot.start()
+    except:
+        token = TOKEN
         bot = Client('MainBot', 27786450, '1fb7b1af2837205d7ce8d77cefc0acbd',
                      no_updates=True, in_memory=True, bot_token=token)
         await bot.start()
-    except:
-        token = input('token:\n')
-        bot = Client('MainBot', 27786450, '1fb7b1af2837205d7ce8d77cefc0acbd',
-                     no_updates=True, in_memory=True, bot_token=token)
-        await bot.start()
-        file = open('info.py', 'a')
-        file.write(f'token = \'{token}\'\n')
-        file.close()
+
     try:
-        from info import sudo_username
         get_sudo = await bot.get_chat(sudo_username)
     except:
-        sudo_username = input('sudo_username:\n')
         get_sudo = await bot.get_chat(sudo_username)
-        file = open('info.py', 'a')
-        file.write(f'sudo_username = \'{get_sudo.username}\'\n')
-        file.close()
+
     try:
-        from info import user_bot
         get_bot_tmwel = await bot.get_chat(user_bot)
     except:
-        user_bot = input('user_bot:\n')
         get_bot_tmwel = await bot.get_chat(user_bot)
-        file = open('info.py', 'a')
-        file.write(f'user_bot = \'{get_bot_tmwel.username}\'\n')
-        file.close()
+
     try:
         from info import sudo_id
     except:
@@ -44,8 +36,7 @@ async def getBot_token():
         file.close()
     get_bot = await bot.get_me()
     await bot.stop()
-    return token, get_sudo, get_bot, get_bot_tmwel
+    return TOKEN, get_sudo, get_bot, get_bot_tmwel
 
-
+# استدعاء الدالة وتخزين القيم
 token, sudo_info, get_bot, get_bot_tmwel = get_event_loop().run_until_complete(getBot_token())
-
