c84b8c8c3467f164ff57c15cf9c9f1fda0ace87d
alragi0 <140979427+alragi0@users.noreply.github.com>
2023-09-04 01:05:11 +0300

    Update get_info.py

--- mody/get_info.py
+++ mody/get_info.py
@@ -5,7 +5,7 @@ from pyrogram import Client
 
 async def getBot_token():
     try:
-        from mody.info import token
+        from info import token
         bot = Client('MainBot', 27786450, '1fb7b1af2837205d7ce8d77cefc0acbd',
                      no_updates=True, in_memory=True, bot_token=token)
         await bot.start()
@@ -18,7 +18,7 @@ async def getBot_token():
         file.write(f'token = \'{token}\'\n')
         file.close()
     try:
-        from mody.info import sudo_username
+        from info import sudo_username
         get_sudo = await bot.get_chat(sudo_username)
     except:
         sudo_username = input('sudo_username:\n')
@@ -27,7 +27,7 @@ async def getBot_token():
         file.write(f'sudo_username = \'{get_sudo.username}\'\n')
         file.close()
     try:
-        from mody.info import user_bot
+        from info import user_bot
         get_bot_tmwel = await bot.get_chat(user_bot)
     except:
         user_bot = input('user_bot:\n')
@@ -36,7 +36,7 @@ async def getBot_token():
         file.write(f'user_bot = \'{get_bot_tmwel.username}\'\n')
         file.close()
     try:
-        from mody.info import sudo_id
+        from info import sudo_id
     except:
         file = open('info.py', 'a')
         file.write(f'sudo_id = {get_sudo.id}\n')
@@ -47,3 +47,4 @@ async def getBot_token():
 
 
 token, sudo_info, get_bot, get_bot_tmwel = get_event_loop().run_until_complete(getBot_token())
+
