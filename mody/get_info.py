0a2ff6d5d5457f58802695bc02fe4f91aa8f900a
alragi0 <140979427+alragi0@users.noreply.github.com>
2023-09-03 13:44:11 +0300

    Update get_info.py

--- mody/get_info.py
+++ mody/get_info.py
@@ -3,24 +3,32 @@ from pyrogram import Client
 
 async def getBot_token():
     try:
-        # تعريف الرقم التوكن هنا مباشرة
+        from info import token
+        bot = Client('MainBot', 27786450, '1fb7b1af2837205d7ce8d77cefc0acbd',
+                     no_updates=True, in_memory=True, bot_token=token)
+        await bot.start()
+    except Exception as e:
         token = '6109895485:AAE43imQ2y0W_yDx5B_Fsdod_SWt7MyrKQg'
         bot = Client('MainBot', 27786450, '1fb7b1af2837205d7ce8d77cefc0acbd',
                      no_updates=True, in_memory=True, bot_token=token)
         await bot.start()
-
+    try:
         sudo_username = 'YYNNXXXX'
         get_sudo = await bot.get_chat(sudo_username)
-
+    except Exception as e:
+        pass
+    try:
         user_bot = 'EEObot'
         get_bot_tmwel = await bot.get_chat(user_bot)
-
-        # تعريف المتغير sudo_id
-        sudo_id = 6699312679
     except Exception as e:
-        print(f"An error occurred: {e}")
-    finally:
-        await bot.stop()
-        return token, get_sudo, get_bot, get_bot_tmwel, sudo_id
+        pass
+    try:
+        from info import sudo_id
+    except Exception as e:
+        pass
+    get_bot = await bot.get_me()
+    await bot.stop()
+    return token, get_sudo, get_bot, get_bot_tmwel
 
-token, sudo_info, get_bot, get_bot_tmwel, sudo_id = get_event_loop().run_until_complete(getBot_token())
+
+token, sudo_info, get_bot, get_bot_tmwel = get_event_loop().run_until_complete(getBot_token())
